ad6ab93791e307dda156d3dc578d16fea855aa43
DavidIbk <98756554+DavidIbk@users.noreply.github.com>
2022-04-03 15:14:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+function navSlide() {
+  var burger = document.querySelector(".burger"); //This gets the burger ICON
+  var nav = document.querySelector(".nav-links"); //this gets all the nav-links and the button too
+  var navLinks = document.querySelectorAll(".nav-links li, button"); //this gets all the items inside our navbar; the links and the button
+
+  //TOGGLE OUR NAVBAR (I.E. INVOKE DROPDOWN)
+  burger.addEventListener("click", function() {
+    nav.classList.toggle("nav-active") //this code instructs the browser to add the class, "nav-active" to the nav-links (because it is that class that has transform: translateX(100%) ) when the burger is clicked, therefore bringing the dropdown back onscreen.
+
+    //ANIMATING OUR NAVLINKS INTO THE DROPDOWN (Here we make reference to our @keyframes)
+    navLinks.forEach((link, index) => { //This syntax just means that we are creating a function that will apply to each nav-links list item and button. Index = 9 (number of links plus button)
+      if (link.style.animation) {
+        link.style.animation = "" //if the links already have animation, then we leave them as they are but if they don't, then the code below runs
+      } else {
+        link.style.animation = "navLinkFade 0.5s ease forwards 0.5s"; //Basically, the code here implements the CSS's navLinkFade animation. Just ensure to type it in the way it is. 0.5s stands for the speed with which the links come to the center. forwards makes the links stay (with backwards they disappear after showing). ease has no noticeable function and 1s is the time that pass before the links appear.
+      }
+    });
+
+    //ANIMATING OUR BURGER ICON UPON BEING CLICKED
+    burger.classList.toggle("toggle");
+
+  });
+
+
+
+}
+
+navSlide(); //This invokes the function above
+
+
+//ANIMATING OUR LAST WEEK SERMON SECTION UPON SCROLL
+window.addEventListener("scroll", ()=>{
+  let content = document.querySelector(".last-week-sermon");
+  let contentPosition = content.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight * 1.2;
+
+  if(contentPosition < screenPosition) {
+    content.classList.add("last-week-sermon-active");
+  }
+})
+
+
+
+//CREATING AN IMAGE GALLERY LIGHTBOX FOR OUR GALLERY SECTION
 //getting all required elements
 const gallery  = document.querySelectorAll(".image"),
 previewBox = document.querySelector(".preview-box"),
